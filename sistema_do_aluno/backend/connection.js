204e3ff3a9e966c8406d92a23b7bdf8e713bfb4d
João Victor <joaovictordsantosr@gmail.com>
2024-10-30 15:49:08 -0300

    Refatorando a função de conexão

--- sistema_do_aluno/backend/connection.js
+++ sistema_do_aluno/backend/connection.js
@@ -1,35 +1,31 @@
 import oracledb from "oracledb";
-oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT; // Correção aqui
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 require("dotenv").config();
 
 async function connectionbd() {
   let connect;
+  if (
+    !process.env.USER ||
+    !process.env.PASSWORD ||
+    !process.env.CONNECTSTRING
+  ) {
+    console.error("Faltam variáveis de ambiente.");
+    process.exit(1);
+  }
 
   try {
     connect = await oracledb.getConnection({
-      user: process.env.SER,
+      user: process.env.USER,
       password: process.env.PASSWORD,
       connectString: process.env.CONNECTSTRING,
     });
 
     console.log("Conectado ao banco de dados");
 
-    const data = await connect.execute(`SELECT * FROM alunos`);
-    console.log(data.rows);
+    return connect;
   } catch (err) {
     console.error(err);
-  } finally {
-    if (connect) {
-      try {
-        await connect.close(); // Fecha a conexão no final
-        console.log("Conexão encerrada.");
-      } catch (err) {
-        console.error("Erro ao fechar a conexão:", err);
-      }
-    }
   }
 }
-
-connectionbd();
 
 module.exports = connectionbd;
