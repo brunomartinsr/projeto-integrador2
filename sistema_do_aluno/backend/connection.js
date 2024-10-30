41ff379c4a5738187b5f283dd777648cf1d4aa57
João Victor <joaovictordsantosr@gmail.com>
2024-10-30 15:18:47 -0300

    Adicionando .env para o projeto

--- sistema_do_aluno/backend/connection.js
+++ sistema_do_aluno/backend/connection.js
@@ -1,35 +1,33 @@
-import oracledb from 'oracledb';
+import oracledb from "oracledb";
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT; // Correção aqui
+require("dotenv").config();
 
 async function connectionbd() {
-    let connect;
+  let connect;
 
-    try {
-        connect = await oracledb.getConnection({
-            user: "nome",
-            password: "senha",
-            connectString: "string"
-        });
-        
-        console.log("Conectado ao banco de dados")
+  try {
+    connect = await oracledb.getConnection({
+      user: process.env.SER,
+      password: process.env.PASSWORD,
+      connectString: process.env.CONNECTSTRING,
+    });
 
-        const data = await connect.execute(
-            `SELECT * FROM alunos`
-        );
-        console.log(data.rows);
+    console.log("Conectado ao banco de dados");
 
-    } catch (err) {
-        console.error(err);
-    } finally {
-        if (connect) {
-            try {
-                await connect.close(); // Fecha a conexão no final
-                console.log("Conexão encerrada.");
-            } catch (err) {
-                console.error("Erro ao fechar a conexão:", err);
-            }
-        }
+    const data = await connect.execute(`SELECT * FROM alunos`);
+    console.log(data.rows);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connect) {
+      try {
+        await connect.close(); // Fecha a conexão no final
+        console.log("Conexão encerrada.");
+      } catch (err) {
+        console.error("Erro ao fechar a conexão:", err);
+      }
     }
+  }
 }
 
 connectionbd();
