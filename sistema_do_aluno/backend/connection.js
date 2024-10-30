117c7c13c6a022b004b7de20d590cf1a8262ad63
João Victor <joaovictordsantosr@gmail.com>
2024-10-30 20:48:13 -0300

    Conectando ao banco oracle

--- sistema_do_aluno/backend/connection.js
+++ sistema_do_aluno/backend/connection.js
@@ -1,30 +1,24 @@
-import oracledb from "oracledb";
+const oracledb = require("oracledb");
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 require("dotenv").config();
 
+if (!process.env.USER || !process.env.PASSWORD || !process.env.CONNECTSTRING) {
+  console.error("Faltam variáveis de ambiente.");
+  process.exit(1);
+}
+
 async function connectionBd() {
   let connect;
-  if (
-    !process.env.USER ||
-    !process.env.PASSWORD ||
-    !process.env.CONNECTSTRING
-  ) {
-    console.error("Faltam variáveis de ambiente.");
-    process.exit(1);
-  }
-
   try {
     connect = await oracledb.getConnection({
       user: process.env.USER,
       password: process.env.PASSWORD,
       connectString: process.env.CONNECTSTRING,
     });
-
     console.log("Conectado ao banco de dados");
-
-    return connect;
+    return connect; // Retorna a conexão para ser usada
   } catch (err) {
-    console.error(err);
+    console.error("Erro ao conectar:", err);
   }
 }
 
