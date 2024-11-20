055fe79af3ae0104e66cf7d92bfe93c6dd17df85
João Victor <joaovictordsantosr@gmail.com>
2024-11-20 13:31:04 -0300

    Formatação de dados para catraca

--- Sistema_catraca/backend/routes.js
+++ Sistema_catraca/backend/routes.js
@@ -1,34 +1,44 @@
 import express from "express";
 import connectionBd from "../../bd/connection.js";
 import { getAlunoByCpf } from "../../services/querys.js";
+import { formatTime, formatDate } from "../../services/formaters.js";
 
 const router = express.Router();
 const conn = await connectionBd();
 
 router.post("/registrar", async (req, res) => {
   const { cpf, hora_entrada, hora_saida, data_registro } = req.body;
+  const entrada = new Date(hora_entrada);
+  const saida = new Date(hora_saida);
+  const dataRegistro = new Date(data_registro);
 
   if (!checkCPF(cpf) || getAlunoByCpf(cpf, conn) == null) {
     res.status(400).send("CPF inválido ou não cadastrado.");
     return;
-  } else if (hora_entrada >= hora_saida) {
+  } else if (entrada >= saida) {
     res
       .status(400)
       .send("As horas de entrada e saídas fornecidas estão erradas.");
     return;
   }
-  const horaTotal = hora_saida - hora_entrada;
+
+  const horaEntradaFormated = formatTime(entrada);
+  const horaSaidaFormated = formatTime(saida);
+  const dataRegistroFormated = formatDate(dataRegistro);
+
   try {
     await conn.execute(
-      `INSERT INTO REGISTROS_CATRACA (CPF_ALUNO, HORA_ENTRADA, HORA_SAIDA, HORA_TOTAL, DATA_REGISTRO) VALUES (:cpf, :hora_entrada, :hora_saida, :hora_total, TO_DATE(:data_registro, 'YYYY-MM-DD'))`,
-      [cpf, hora_entrada, hora_saida, horaTotal, data_registro],
+      `INSERT INTO REGISTROS_CATRACA (CPF_ALUNO, HORA_ENTRADA, HORA_SAIDA, DATA_REGISTRO) VALUES (:cpf, TO_DATE(:hora_entrada, 'HH24:MI:SS'), TO_DATE(:hora_saida, 'HH24:MI:SI'), TO_DATE(:data_registro, 'YYYY-MM-DD'))`,
+      [cpf, horaEntradaFormated, horaSaidaFormated, dataRegistroFormated],
       { autoCommit: true }
     );
     res.status(200).json({ message: "Acesso registrado com sucesso." });
   } catch (err) {
     console.error("Erro ao registrar acesso: ", err);
-    res.status;
+    res.status(500).send("Erro ao registrar acesso: " + err.message);
   } finally {
     conn.close();
   }
 });
+
+export default router;
--- bd/server.js
+++ bd/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import router_aluno from "../sistema_do_aluno/backend/routes.js";
+import router_catraca from "../sistema_catraca/backend/routes.js";
 
 const app = express();
 const PORT = 3000;
@@ -11,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use("/aluno", router_aluno);
-// app.use("/catraca", router_catraca);
+app.use("/catraca", router_catraca);
 // app.use("/gerente", router_gerente);
 
 app.listen(PORT, () => {
